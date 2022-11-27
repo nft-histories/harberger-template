3696249d06f9de6b6fb2d84da19362cbc6dddf2c
lazaralex98 <alex2012ro@gmail.com>
2022-11-27 20:11:04 +0200

    chore: linter, prettier, solhint

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,10 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import '@nomiclabs/hardhat-solhint';
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.17",
+    version: '0.8.17',
     settings: {
       optimizer: {
         enabled: true,
